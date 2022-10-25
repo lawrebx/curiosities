a0dd291a780f1e2c2be5a188288b2efdaa58a161
Brandon Lawrence <lawrebx@gmail.com>
2022-10-25 00:50:54 -0500

    manim upload

---
+++ ReinforcementLearningToy/rltoy.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+class rl_grid:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def generate_grid():
+        width = self.width
+        height = self.height
